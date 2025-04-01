26e3ddaa22f1fe5de65fc239368acc0711c3c5d8
mj <mjmartinsu@yahoo.es>
2025-04-01 13:22:23 +0200

    Create a new component for btn Habla - communicatons

---
+++ dist/src/components/comunications.ts
@@ -0,0 +1,18 @@
+import { Character } from '../types/character';
+import { render } from './base.js';
+
+
+
+export function createCommunications(
+  selector = 'body',
+  position: InsertPosition = 'beforeend',
+  character: Character,
+) {
+  const template = /*html*/ 'h';
+  <div class="communications">
+    <p class="text">Una frase que dice alguien</p>
+    <img class="picture" src="./assets/no-one.jpg" alt="Nombre y familia del que habla" />
+  </div>
+  console.log(element);
+}
+
